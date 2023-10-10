317cbba3fee938e5f339899a35a026002c70bc6b
debashish_github <webshack2018@icloud.com>
2023-10-10 14:37:10 +0530

    10oct2023 class

---
+++ classroom practices/assign10.cpp
@@ -0,0 +1,7 @@
+// write a c++ program to save the cgpa and sgpa information of each semester using a class called 'semester' and static data member
+
+// course credit -
+// 1st 20
+// 2nd 30
+// 3rd 20
+// 4th 30
---
+++ classroom practices/testmca1010.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class item
+{
+    static int count; // declaration
+    int number;
+
+public:
+    void getdata(int a)
+    {
+        number = a;
+        count++;
+    }
+    void getcount()
+    {
+        cout << "\n Count";
+        cout << count << "\n";
+    }
+};
+int item::count; // definition
+int main()
+{
+    item a; // count 0
+    a.getcount();
+    item b, c;
+    b.getcount();
+    c.getcount();
+    a.getdata(1);
+    b.getdata(2);
+    c.getdata(3);
+    a.getdata(44);
+    cout << "\n After data entry: ";
+    a.getcount();
+    b.getcount();
+    c.getcount();
+    return 0;
+}
+
+// write a c++ program to save the cgpa and sgpa info of each sem. using a class called 'semester' and static data member
